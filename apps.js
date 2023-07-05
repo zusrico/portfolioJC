7efdf5d5cdaf484a9c791c715a3d9fe586ff6e53
zus <jesus.garciasanchez.dev@gmail.com>
2023-07-05 20:11:46 +0200

    Inicio final diseño

--- apps.js
+++ apps.js
@@ -1,7 +1,10 @@
 
 
-  var imagen_primera = document.getElementsByClassName("lazyto");
+var imagen_primera = document.getElementsByClassName("lazyto");
 var tarjeta_texto = document.getElementById("fotito");
+
+var tarjeta_trabajo_diseno = document.getElementsByClassName("tarjeta_trabajo_diseno");
+var tarjeta_diseno = document.getElementsByClassName("tarjeta_diseno");
 
 
 
@@ -14,8 +17,27 @@ var tarjeta_texto = document.getElementById("fotito");
   }
 
 for (let i = 0; i< imagen_primera.length; i++){
-    imagen_primera[i].addEventListener("mouseenter", efectoBlur);
-    imagen_primera[i].addEventListener("mouseleave", quitarBlur);
+  imagen_primera[i].addEventListener("mouseenter", efectoBlur);
+  imagen_primera[i].addEventListener("mouseleave", quitarBlur);
 }
     
 
+function infoDisenoAparece(){
+    tarjeta_diseno[j].style.display = "flex";
+}
+
+function infoDisenoDesaparece(){
+    tarjeta_diseno[j].style.display = "none";
+}
+
+for (let j = 0; j<tarjeta_diseno.length; j++){
+  tarjeta_trabajo_diseno[j].addEventListener("mouseenter", infoDisenoAparece);
+  tarjeta_trabajo_diseno[j].addEventListener("mouseleave", infoDisenoDesaparece);
+}
+
+
+var tarjeta_diseno_proyecto = document.getElementsByClassName("diseno_articulo");
+
+function expandeLinea(){
+  
+}
